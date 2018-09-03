135076867a44f8f4f41b9490f8af0b3bd0aab95f
Jacob Larsen <jacobniclarsen@gmail.com>
2018-09-03 13:57:26 +0200

    Oppgave 1 forste del ferdig

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -1,8 +1,17 @@
 package no.oslomet.cs.algdat;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] b = new int[0];
+
+        int maks = Oblig1.max(b);
+
+        System.out.println(maks);
+        System.out.println(Arrays.toString(b));
+
     }
 }
--- src/no/oslomet/cs/algdat/Oblig1.java
+++ src/no/oslomet/cs/algdat/Oblig1.java
@@ -1,4 +1,69 @@
 package no.oslomet.cs.algdat;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
 public class Oblig1 {
+
+    public Oblig1(){};
+
+    /**
+     * Finner maks verdien i en Array
+     * @param a Array
+     * @return Makes verdi
+     */
+    public static int max(int[] a){
+        if (a.length == 0){
+            throw new NoSuchElementException("Tom Array");
+        }
+
+        for(int i = 0; i < a.length - 1; i++){
+
+
+            if(a[i] > a[i+1]){
+                bytt(a,i,i+1);
+            }
+
+        }
+        int maks = a[a.length - 1];
+        return maks;
+    }
+    //Det blir flest ombyttinger om tabellen er sortert descending / Synkene
+    //Det blir faerrest ombyttinger om tabellen er sortert ascending/ Stigene
+    //I snitt blir det Log(n) + 0.577 ombyttninger, det gunstige tallet
+
+    /**
+     * Bytter 2 tall i en array basert på index
+     * @param a Array
+     * @param i Index 1
+     * @param j Index 2
+     */
+    public static void bytt(int[] a, int i, int j){
+
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    /**
+     * lager en tilfeldig perumert Array
+     * @param n Antall verdier i tabellen
+     * @return
+     */
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
+
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
 }
